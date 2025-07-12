a041b439f46ba34d0f14e7e51dbb46ffb34ec1b4
amartyawc <amartya.dey@wecredit.co.in>
2025-07-12 17:45:21 +0530

    Added some comment

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+
 	"github.com/wecredit/prometheus-sdk/config"
 	"github.com/wecredit/prometheus-sdk/metrics"
-	"github.com/wecredit/prometheus-sdk/server"
+	routes "github.com/wecredit/prometheus-sdk/server"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
+	// Load the configuration from the specified JSON file
 	err := config.LoadConfig("sdk_config.json")
 	if err != nil {
-		log.Fatalf("Config error: %v", err)
+		log.Fatalf("Config error: %v", err) // Log and exit if there is a configuration error
 	}
 
+	// Initialize the metrics system
 	metrics.Init()
 
+	// Register the Prometheus metrics handler
 	http.Handle("/metrics", promhttp.Handler())
+
+	// Register application-specific routes
 	routes.RegisterRoutes()
 
+	// Log the server start message and listen on the configured port
 	log.Printf("Prometheus SDK API running on :%s", config.Cfg.MetricsPort)
-	log.Fatal(http.ListenAndServe(":"+config.Cfg.MetricsPort, nil))
+	log.Fatal(http.ListenAndServe(":"+config.Cfg.MetricsPort, nil)) // Start the HTTP server
 }
